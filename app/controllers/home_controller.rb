1fb8058b016f2062b5ac6503d8cdc6008e0dd069
Roy Ketelaar <royketelaar@gmail.com>
2013-11-30 15:10:00 +0100

    redirects to dashboard if user logged in and redirects to home is user is not logged in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
 	def index
-		redirect_to :controller=>'dashboard', :action=> 'index'
+		if user_signed_in?
+			redirect_to :controller=>'dashboard', :action=> 'index'
+		end
 	end
 end
