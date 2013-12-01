442e8a164bbf5a3520a45f38aca5cae9f7c3d4be
Rémon Wobbema <r.wobbema@me.com>
2013-12-01 15:01:05 +0100

    progressie balk op profielpagina werkt!

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  
-  validates :first_name, presence: true, :allow_nil => true
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
